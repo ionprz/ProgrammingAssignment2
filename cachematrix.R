c62b03163cd5f3bac147c220a59a037192a467c0
ionprz <82572880+ionprz@users.noreply.github.com>
2021-04-17 14:22:02 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-The goal of work is to create two functions, "makeCacheMatrix" and "cacheSolve," that stock the opposite of a matrix.
+The goal of work is to create two functions, "makeCacheMatrix" and "cacheSolve," that stock the opposite of a matrix.
+
 ## Write a short comment describing this function
 makeCacheMatrix is a function that generates an unique "matrix" object capable of caching its opposite.
+
 makeCacheMatrix <- function(x = matrix()) {
-inv <- NULL
-set <- function(y) {
-x <<- y
-inv <<- NULL
+	inv <- NULL
+	set <- function(y) {
+	x <<- y
+	inv <<- NULL
 }
-get <- function() x
-setinv <- function(inverse) inv <<- inverse
-getinv <- function() inv
-list(set = set, get = get, setinv = setinv, getinv = getinv)
+	get <- function() x
+	setInverse <- function(inverse) inv <<- inverse
+	getInverse <- function() inv
+	list(set = set,get = get,setInverse = setInverse,getInverse = getInverse)
 }
 
 ## Write a short comment describing this function
 cacheSolve is a function that calculates the reciprocal of a particular "matrix."
-makeCacheMatrix displays a cache matrix If the opposite has already been computed, the cacheSolve function can recover it from the cache
+makeCacheMatrix displays a cache matrix If the opposite has already been computed, 
+the cacheSolve function can recover it from the cache
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
-inv <- x$getinv()
-if(!is.null(inv)) {
-message("getting cached result")
-return(inv)
 }
-data <- x$get()
-inv <- solve(data, ...)
-x$setinv(inv)
-inv
-}
+
+##  Checking
+
+my_matrix <- makeCacheMatrix(matrix(1:6, 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+## NULL
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+my_matrix$set(matrix(c(1, 3, 2, 4), 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+#NULL
+cacheSolve(my_matrix)
+my_matrix$getInverse()
